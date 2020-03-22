2ebcaf1a271d793a549b9a8a0a0241238b2d852c
Matthew Isabel <matthewisabel@github.com>
2020-03-22 19:03:32 -0400

    June haikus update

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const http = require('http');
+let express = require('express');
+let app = express();
+let ejs = require('ejs');
+const haikus = require('./haikus.json');
+const port = process.env.PORT || 1337;
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello there!");
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {haikus: haikus});
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(port);
