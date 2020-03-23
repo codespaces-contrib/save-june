e65ef20fff72e85e972cf5a6ceff37d1c844fa97
Matthew Isabel <matthewisabel@github.com>
2020-03-23 10:53:08 -0400

    Add .devcontainer

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let ejs = require('ejs');
 const haikus = require('./haikus.json');
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
