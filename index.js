a8fdf1aeb5a378eb29bcda0884c8afccf5506b1d
Matthew Isabel <matthewisabel@github.com>
2020-03-30 16:38:40 -0400

    Revert "Move to docker-compose and postgres"
    This reverts commit 02da269a04adac65d230cec9a3f38683d60268d0.

--- index.js
+++ index.js
@@ -3,32 +3,12 @@ let app = express();
 let ejs = require('ejs');
 const haikus = require('./haikus.json');
 const port = process.env.PORT || 3000;
-const { Pool } = require('pg')
 
-const pool = new Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.host,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.port
-});
-
-
-app.use(express.json());
-app.use(express.urlencoded());
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  pool.query('SELECT * FROM haikus ORDER BY id', (err, haikus) => {
-    res.render('index', {haikus: haikus.rows});
-  });
-});
-
-app.post('/heart', (req, res) => {
-  pool.query('UPDATE haikus SET hearts = hearts + 1 WHERE id = $1', [req.body.id], (err, haikus) => {
-    res.send('Success');
-  });
+  res.render('index', {haikus: haikus});
 });
 
 app.listen(port);
