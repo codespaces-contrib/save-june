c077efd1d6c6603cda53f7df075bec279b70af46
Matthew Isabel <matthewisabel@github.com>
2020-05-19 18:07:44 -0400

    Add docker-compose

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ let app = express();
 let ejs = require('ejs');
 const haikus = require('./haikus.json');
 const port = process.env.PORT || 3000;
-const url = 'http://locahost'|| process.env.stagingURL || process.env.prodURL
+const { Pool } = require('pg')
 
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.HOST,
+  port: 5432
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  pool.query('SELECT * FROM haikus ORDER BY id', (err, haikus) => {
+    res.render('index', {haikus: haikus.rows});
+  });
+});
+
+app.post('/heart', (req, res) => {
+  pool.query('UPDATE haikus SET hearts = hearts + 1 WHERE id = $1', [req.body.id], (err, haikus) => {
+    res.send('Success');
+  });
 });
 
 app.listen(port);
-
-console.log(`Server running on ${url}:${port}`);
