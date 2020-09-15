81548dcd5a685946dbaf3292fdb55af993e6b48b
Jonathan Carter <jc@trl.mx>
2020-09-15 17:22:26 +0000

    Misc. fixes

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-let express = require('express');
-let app = express();
-let ejs = require('ejs');
-const haikus = require('./haikus.json');
+const express = require("express");
+const { Pool } = require("pg");
+
 const port = process.env.PORT || 3000;
-const { Pool } = require('pg')
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  host: 'db',
-  port: 5432
+  host: "db",
+  port: 5432,
 });
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'))
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  pool.query('SELECT * FROM haikus ORDER BY id', (err, haikus) => {
-    res.render('index', {haikus: haikus.rows});
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM haikus ORDER BY id", (_, haikus) => {
+    res.render("index", { haikus: haikus.rows });
   });
 });
 
-app.post('/heart', (req, res) => {
-  pool.query('UPDATE haikus SET hearts = hearts + 1 WHERE id = $1', [req.body.id], (err, haikus) => {
-    res.send('Success');
-  });
+app.post("/heart", (req, res) => {
+  pool.query(
+    "UPDATE haikus SET hearts = hearts + 1 WHERE id = $1",
+    [req.body.idz],
+    () => res.send("Success")
+  );
 });
 
-app.listen(port);
-console.log('Server running on','https://localhost:3000')
+app.listen(port, () =>
+  console.log("Server running on: ", "http://localhost:3000")
+);
