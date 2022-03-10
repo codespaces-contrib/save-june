599f4092c3943b08341d3fb7cac620a4098b816b
Jonathan Carter <joncart@microsoft.com>
2022-03-09 16:05:49 -0800

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,5 @@
 const { Pool } = require("pg");
 
 exports.createDb = () => {
-  return new Pool({
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    host: "db",
-    port: 5432,
-  });
+  return new Pool();
 };
