df5efb57e2e0a4f240b826abf6be35da7d87de4f
Chuck Lantz <clantz@microsoft.com>
2022-03-11 11:15:13 -0800

    Updates

--- worker/main.go
+++ worker/main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.Ho{
+		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
